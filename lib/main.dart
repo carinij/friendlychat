3839873ed26db09ca16ea9eb5dd508ff8b015f63
Jason Carini <carinij@gmail.com>
2019-08-22 21:52:43 -0700

    First steps

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,52 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+  @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Friendlychat")),
+      body: _buildTextComposer(),
     );
   }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Send a message'
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal:4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
 }
+
